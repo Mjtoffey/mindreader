166ed5f6e730b1f969ccf199b093294aaa1e5e11
Mjtoffey <mjtoffey@gmail.com>
2023-06-08 16:55:07 -0400

    updated main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,9 @@
-
-const webPage = document.getElementById ('webPage');
-const info = document.getElementById ("info");
-const next = document.getElementById ("next");
-const sample = document.getElementById ("sample");
-const goRef = document.getElementById ("goRef");
+document.addEventListener("load", () => {
+    const webPage = document.getElementById('webPage');
+const info = document.getElementById("info");
+const nextBtn = document.getElementById("next");
+const sample = document.getElementById("sample");
+const goRef = document.getElementById("goRef");
 
 let symbols = [
     '~',
@@ -14,41 +14,52 @@ let symbols = [
     '%',
     '^',
     '&',
-    '*',
-    '+',
+    '*'
 ]
 
-const pages = [ 
+let pages = [ 
 {
-    info.textContent = ('I can read your mind'),
-    goRef.textContent = ("Go"),
-}
+    info: info.textContent = 'I can read your mind',
+    goRef: goRef.textContent = 'Go',
+},
+{ 
+    info: info.textContent = 'Pick a number from 01-99',
+    nextBtn: next.textContent = 'next',
+    sample: sample.textContent = 'when you have your number click next',
+    goRef: goRef.textContent = img
+},
 {
-    info.textContent = ('Pick a number from 01-99'),
-    next.textContent = ('next'),
-    sample.textContent = ('when you have your number click next'),
-    goRef.textContent = img
-}
+    info: info.textContent = 'Add both digits together to get a new number',
+    nextBtn: next.textContent = 'next',
+    sample: sample.textContent = 'Ex; 14 is 1 + 4 = 5,\n click next to proceed',
+    goRef: goRef.textContent = img
+},
 {
-    info.textContent = ('Add both digits together to get a new number'),
-    next.textContent = ('next'),
-    sample.textContent = ('Ex; 14 is 1 + 4 = 5, click next to proceed'),
-    goRef.textContent = img
-}
-{
-    info.textContent = ('Subtract your new number from the original number'),
-    next.textContent = ('next'),
-    sample.textContent = ('Ex; 14 - 5 = 9, click next to proceed'),
-    goRef.textContent = img
-}
+    info: info.textContent = 'Subtract your new number from the original number',
+    nextBtn: next.textContent = 'next',
+    sample: sample.textContent = 'Ex; 14 - 5 = 9,\n click next to proceed',
+    goRef: goRef.textContent = img
+},
 {//disoplay random list from array
-    info.textContent = (''),
-    next.textContent = ('reveal'),
-    sample.textContent = ('Find your new number.  Note the symbol beside the number'),
-    goRef.textContent = img
-}
+    // fix info: info.textContent = (''),
+    nextBtn: next.textContent = 'reveal',
+    sample: sample.textContent = 'Find your new number.\n  Note the symbol beside the number',
+    goRef: goRef.textContent = img
+},
 { //symbol
-    info.textContent = (''),
-    sample.textContent = ('Your symbol is :'),
-    goRef.textContent = img
-}]
+    info: info.textContent = '',
+    sample: sample.textContent = 'Your symbol is " " ',
+    goRef: goRef.textContent = img
+}
+]
+let currentPage = pages[0]
+
+nextBtn.addEventListener("click", function() {
+    for (let i = 0; i <= 5; i++){
+        console.log(pages[i])
+        return pages[i];
+        
+    }
+})"
+
+})
