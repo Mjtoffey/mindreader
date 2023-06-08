455d755a7b7c164c56047351958a8e563c3bac09
Mjtoffey <mjtoffey@gmail.com>
2023-06-08 18:07:31 -0400

    updated main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,65 +1,58 @@
-document.addEventListener("load", () => {
-    const webPage = document.getElementById('webPage');
-const info = document.getElementById("info");
-const nextBtn = document.getElementById("next");
-const sample = document.getElementById("sample");
-const goRef = document.getElementById("goRef");
+window.addEventListener("load", () => {
+  let symbols = ["~", "!", "@", "#", "$", "%", "^", "&", "*"];
 
-let symbols = [
-    '~',
-    '!',
-    '@',
-    '#',
-    '$',
-    '%',
-    '^',
-    '&',
-    '*'
-]
-
-let pages = [ 
-{
-    info: info.textContent = 'I can read your mind',
-    goRef: goRef.textContent = 'Go',
-},
-{ 
-    info: info.textContent = 'Pick a number from 01-99',
-    nextBtn: next.textContent = 'next',
-    sample: sample.textContent = 'when you have your number click next',
-    goRef: goRef.textContent = img
-},
-{
-    info: info.textContent = 'Add both digits together to get a new number',
-    nextBtn: next.textContent = 'next',
-    sample: sample.textContent = 'Ex; 14 is 1 + 4 = 5,\n click next to proceed',
-    goRef: goRef.textContent = img
-},
-{
-    info: info.textContent = 'Subtract your new number from the original number',
-    nextBtn: next.textContent = 'next',
-    sample: sample.textContent = 'Ex; 14 - 5 = 9,\n click next to proceed',
-    goRef: goRef.textContent = img
-},
-{//disoplay random list from array
-    // fix info: info.textContent = (''),
-    nextBtn: next.textContent = 'reveal',
-    sample: sample.textContent = 'Find your new number.\n  Note the symbol beside the number',
-    goRef: goRef.textContent = img
-},
-{ //symbol
-    info: info.textContent = '',
-    sample: sample.textContent = 'Your symbol is " " ',
-    goRef: goRef.textContent = img
-}
-]
-let currentPage = pages[0]
-
-nextBtn.addEventListener("click", function() {
-    for (let i = 0; i <= 5; i++){
-        console.log(pages[i])
-        return pages[i];
-        
+  let pages = [
+    {
+      info: "I can read your mind",
+      actionBtnLabel: null,
+      description: null,
+    },
+    {
+      info: "Pick a number from 01-99",
+      actionBtnLabel: "next",
+      description: "when you have your number click next",
+    },
+    {
+      info: "Add both digits together to get a new number",
+      actionBtnLabel: "next",
+      description: "Ex; 14 is 1 + 4 = 5,\n click next to proceed",
+    },
+    {
+      info: "Subtract your new number from the original number",
+      actionBtnLabel: "next",
+      description: "Ex; 14 - 5 = 9,\n click next to proceed",
+    },
+    {
+      //disoplay random list from array
+      info: null,
+      actionBtnLabel: "reveal",
+      description: "Find your new number.\n  Note the symbol beside the number",
+    },
+    {
+      info: null,
+      actionBtnLabel: null,
+      description: 'Your symbol is " " ', //symbols
+    },
+  ];
+  let currentPage = 0;
+  document.getElementById("actionBtn").addEventListener("click", function () {
+    if (currentPage < pages.length - 1) {
+      currentPage++;
     }
-})"
+    renderPage(pages[currentPage]);
+  });
+});
+function renderPage(pageObj) {
+  const webPage = document.getElementById("webPage");
+  const info = document.getElementById("info");
+  const actionBtn = document.getElementById("actionBtn");
+  const description = document.getElementById("description");
+  const goRef = document.getElementById("goRef");
 
-})
+  actionBtn.textContent = pageObj.actionBtnLabel;
+  description.textContent = pageObj.description;
+  info.textContent = pageObj.info;
+  if (pageObj.actionBtnLabel === null) {
+    actionBtn.classList.add("hide-button");
+  }
+}
